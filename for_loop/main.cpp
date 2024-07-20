6e73d7847b08a71defb89d46fff81a61da566d7f
Tefo Tsiu <tefots>
2024-07-20 12:04:07 +0200

    adding while and do while loops

--- for_loop/main.cpp
+++ for_loop/main.cpp
@@ -1,5 +1,7 @@
 // C++ program to Demonstrate for loop
 #include <iostream>
+//include for vectors
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -9,6 +11,7 @@ int main()
 	}
     cout << "\n\n\n";
 	//program to print i am happy 5 times
+    cout<< "Iterating using certain word"<<endl;
 	for (int x = 0; x < 5; x++) {
 
 		cout<<"I am Happy"<<endl;
@@ -16,7 +19,9 @@ int main()
 
 	//for loop in arrays
     int arr[] {40, 50, 60, 70, 80, 90, 100};
+    cout<< "Iterating through array elements"<<endl;
     for (auto elements: arr){
+
     cout << elements << " ";
     //here auto is
     //elements is the name of the variable
@@ -31,10 +36,19 @@ int main()
     }
 
     //complex codes
+    cout<< "FOR LOOP IN THE COMPLEX CODE"<<endl;
 	for (int i = 0, j = 10, k = 20; (i + j + k) < 100;
 		j++, k--, i += k) {
 		cout << i << " " << j << " " << k << "\n";
 	  }
 
+
+    //using for loops in vectors
+    cout<< "FOR LOOP USING VECTOR"<<endl;
+	  vector<int> v = { 1, 2, 3, 4, 5 };
+    for (vector<int>::iterator it = v.begin();
+         it != v.end(); it++) {
+        cout << *it << "\t";
+         }
 	return 0;
 }
